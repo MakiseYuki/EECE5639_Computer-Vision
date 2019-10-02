81d0e1ac06edaaa53c31b8c3e353f701660ec8c4
MakiseYuki <31904890+MakiseYuki@users.noreply.github.com>
2019-10-03 04:22:22 +0900

    main changed

--- Project1/Project_1_S.m
+++ Project1/Project_1_S.m
@@ -25,18 +25,70 @@ for k = 1:total_images
             d1Filt_image(v,h) = (I_gray(v,h-1)*(-1)+I_gray(v,h+1)*1)*0.5;
         end
     end
+    % d1Filt_image is the siple filt operator filt original image without
+    % other filer method
+    
+    %create the gaussian 1D array to apply on image
+    gaussFilt_image = I_gray;
+    gaussFilt = fspecial('gaussian',[1,3],2.0);
+    
+    %use gaussian [1,3] array to apply on the original image without other
+    %filter method
+    for v = 1:240
+        for h = 2:319
+            gaussFilt_image(v,h) = (I_gray(v,h-1)*gaussFilt(1,1))+(I_gray(v,h)*gaussFilt(1,2))+(I_gray(v,h+1)*gaussFilt(1,3));
+        end
+    end
+    
+    %before getting the temporal derivative aplly 3*3 box filter to filt
+    %the original image (smoothing)
+    three_cross_threeFilt_image = imboxfilt(I_gray,3);
+    three_cross_d1Filt_image = three_cross_threeFilt_image;
+    three_cross_gaussFilt_image = three_cross_threeFilt_image;
+    %than apply the simple filter to the the temporal derivative
+    [height,width] = size(three_cross_threeFilt_image);
+    for v = 1:height
+        for h = 2:width-1
+            three_cross_d1Filt_image(v,h) = (three_cross_threeFilt_image(v,h-1)*(-1)+three_cross_threeFilt_image(v,h+1)*1)*0.5;
+        end
+    end
+    
+    %same but filter withe the gaussian filter
+    for v = 1:height
+        for h = 2:width-1
+            three_cross_gaussFilt_image(v,h) = (three_cross_threeFilt_image(v,h-1)*gaussFilt(1,1))+(three_cross_threeFilt_image(v,h)*gaussFilt(1,2))+(three_cross_threeFilt_image(v,h+1)*gaussFilt(1,3));
+        end
+    end
+ 
+    %before getting the temporal derivative aplly 5*5 box filter to filt
+    %the original image (smoothing)
+    five_cross_fiveFilt_image = imboxfilt(I_gray,5);
+    five_cross_d1Filt_image = five_cross_fiveFilt_image;
+    five_cross_gaussFilt_image = five_cross_fiveFilt_image;
+    [height, width] = size(five_cross_fiveFilt_image);
+    for v = 1:height
+        for h = 2:width-1
+            five_cross_d1Filt_image(v,h) = (five_cross_fiveFilt_image(v,h-1)*(-1)+five_cross_fiveFilt_image(v,h+1)*1)*0.5;
+        end
+    end
+    
+    %same but filter withe the gaussian filter
+    for v = 1:height
+        for h = 2:width-1
+            five_cross_gaussFilt_image(v,h) = (five_cross_fiveFilt_image(v,h-1)*gaussFilt(1,1))+(five_cross_fiveFilt_image(v,h)*gaussFilt(1,2))+(five_cross_fiveFilt_image(v,h+1)*gaussFilt(1,3));
+        end
+    end
+    
+    %function for getting the mask
     
     
     
-    tsigma = 4.0;
-    tsigma = double(tsigma);
-    %use gaussian filter
-    gassFilt_image = exp(-I_gray^2/(2*tsigma^2))/(tsigma*sqrt(2*pi));
-    
-    
-    
-    
-    
+    %imshow(five_cross_d1Filt_image)
+    %imshow(three_cross_d1Filt_image)
+    %imshow(five_cross_gaussFilt_image)
+    %imshow(three_cross_gaussFilt_image)
+    %imshow(three_cross_threeFilt_image)
+    %imshow(five_cross_fiveFilt_image)
     %imshow(I_gray)
     %imshow(d1Filt_image)
     %imshow(gaussFilt_image)
